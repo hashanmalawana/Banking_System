b87b29cbdec78cd2c42e6bf03580f451ad26a5f5
Hashan Malawana <hashanmalawana@gmail.com>
2018-05-05 09:50:20 +0530

    optmize

--- show_cus_cid1.php
+++ show_cus_cid1.php
@@ -54,7 +54,7 @@ if (isset($_POST['btn-view'])) {
 }
  else {
 
-echo "<script>alert('Invalid Customer ID or NIC Number!')</script>";
+echo "<script>alert('Sorry,Invalid Customer ID or NIC Number!')</script>";
  }
 
  $DBcon->close();
